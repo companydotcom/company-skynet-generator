955be7244f8983d6d7f0db917205a6a290be578f
Bharath Kishore Aluri <bharathkishore.a@accionlabs.com>
2019-12-02 22:11:29 +0530

    Added ability to store and retrieve service specific user data and fixed eslint issues

--- generator-companyskynet/generators/app/templates/workers/transitionWorker.js
+++ generator-companyskynet/generators/app/templates/workers/transitionWorker.js
@@ -1,19 +1,32 @@
-export default async ({ message, attributes, serviceConfigData }) => {
-  console.log(`transitionWorker: INFO: Received a call to work with message as ${JSON.stringify(message, null, 2)}, attributes as ${JSON.stringify(attributes, null, 2)} and service config data as ${JSON.stringify(serviceConfigData, null, 4)}`);
+export default async ({
+  message, attributes, serviceConfigData, serviceUserData,
+}) => {
+  // Look out for user data, account data, product data in message.context
+  // Service specific config data that was stored during initial setup will be
+  // available in serviceConfigData. Any service specific user data that was
+  // stored earlier will be available in serviceUserData
+  console.log(`transitionWorker: INFO: Received a call to work with message as ${JSON.stringify(message, null, 2)}, attributes as ${JSON.stringify(attributes, null, 2)}, service config data as ${JSON.stringify(serviceConfigData, null, 4)} and service specific user data as ${JSON.stringify(serviceUserData, null, 4)}`);
   // Write all logic to perform the required work. Create other files for other
   // helpers as necessary, import and use them while keeping this file clean.
   // Do not worry about catching unless necessary for the business logic.
 
   // Check if the type of fetch event is defined. Else, throw an error
-  if (typeof message.metadata === 'undefined' || typeof message.metadata.eventType === 'undefined') {
+  if (typeof message.metadata === 'undefined'
+    || typeof message.metadata.eventType === 'undefined') {
     throw new Error('Message did not have the required paameter metadata or parameter eventType within metadata');
   }
 
   // Check what type if fetch event has been requested and perform business
-  // logic based on the same
+  // logic based on the same.
+  // If you would like to save any service specific user data to database and
+  // fetch it later, send a parameter 'serviceUserData' with the data to be saved
+  // as a key in the return value object.
   switch (message.metadata.eventType) {
     case '':
-      return 'Here is where you send the response';
+      return {
+        res: 'Here is where you send the response data. This can also be an object',
+        serviceUserData: {},
+      };
     default:
       throw new Error(`transitionWorker: ERROR: metadata.eventType not recognized: ${message.metadata.eventType}`);
   }
