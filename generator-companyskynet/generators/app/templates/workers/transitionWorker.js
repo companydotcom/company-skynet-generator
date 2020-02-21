40f0d22978ee60105dad3e78b89e3a71d931fe85
Bharath Kishore Aluri <bharathkishore.a@accionlabs.com>
2020-02-21 22:01:44 +0530

    Added ability to get serviceUserData and serviceAccountData.
    Re-coded logging

--- generator-companyskynet/generators/app/templates/workers/transitionWorker.js
+++ generator-companyskynet/generators/app/templates/workers/transitionWorker.js
@@ -1,14 +1,21 @@
 export default async ({
-  message, attributes, serviceConfigData, serviceUserData,
+  message, attributes, serviceConfigData, serviceAccountData, serviceUserData,
 }) => {
   // Look out for user data, account data, product data in message.context
   // Service specific config data that was stored during initial setup will be
-  // available in serviceConfigData. Any service specific user data that was
-  // stored earlier will be available in serviceUserData
-  console.log(`transitionWorker: INFO: Received a call to work with message as ${JSON.stringify(message, null, 2)}, attributes as ${JSON.stringify(attributes, null, 2)}, service config data as ${JSON.stringify(serviceConfigData, null, 4)} and service specific user data as ${JSON.stringify(serviceUserData, null, 4)}`);
+  // available in serviceConfigData. Any service specific user account data that
+  // was stored earlier will be available in serviceAccountData. Similar is the
+  // case with serviceUserData
+  console.log('transitionWorker: INFO: Received a call to work.');
+  console.log(`message => ${JSON.stringify(message, null, 2)})`);
+  console.log(`attributes => ${JSON.stringify(attributes, null, 2)},`);
+  console.log(`serviceConfigData => ${JSON.stringify(serviceConfigData, null, 4)}`);
+  console.log(`serviceAccountData => ${JSON.stringify(serviceAccountData, null, 4)}`);
+  console.log(`serviceUserData => ${JSON.stringify(serviceUserData, null, 4)}`);
+
   // Write all logic to perform the required work. Create other files for other
   // helpers as necessary, import and use them while keeping this file clean.
-  // Do not worry about catching unless necessary for the business logic.
+  // Do not worry about catching errors unless necessary for the business logic.
 
   // Check if the type of fetch event is defined. Else, throw an error
   if (typeof message.metadata === 'undefined'
@@ -16,15 +23,16 @@ export default async ({
     throw new Error('Message did not have the required paameter metadata or parameter eventType within metadata');
   }
 
-  // Check what type if fetch event has been requested and perform business
+  // Check what type of transition event has been requested and perform business
   // logic based on the same.
   // If you would like to save any service specific user data to database and
-  // fetch it later, send a parameter 'serviceUserData' with the data to be saved
-  // as a key in the return value object.
+  // fetch it later, send a parameter 'serviceAccountData' with the data to be saved
+  // as a key in the return value object. Same is the case for 'serviceUserData'
   switch (message.metadata.eventType) {
     case '':
       return {
         res: 'Here is where you send the response data. This can also be an object',
+        serviceAccountData: {},
         serviceUserData: {},
       };
     default:
