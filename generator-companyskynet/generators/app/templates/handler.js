66e317407baa75c422e99e8af3daecb3917bfbaf
Bharath Kishore Aluri <bharathkishore.a@accionlabs.com>
2020-07-27 18:37:38 +0530

    Added HTTP handler to inject requests through HTTP

--- generator-companyskynet/generators/app/templates/handler.js
+++ generator-companyskynet/generators/app/templates/handler.js
@@ -5,6 +5,7 @@ import {
   directTransitionHandler as dTHandler,
   bulkTransitionHandler as bTHandler,
   setupDatabase as sDb,
+  httpReqHandler,
 } from '@companydotcom/company-skynet-core';
 import fMsgHandler from './workers/fetchWorker';
 import tMsgHndlr from './workers/transitionWorker';
@@ -68,4 +69,28 @@ export const bulkTransitionHandler = async event => {
 // eslint-disable-next-line arrow-body-style
 export const setupDatabase = async event => {
   return sDb(AWS, event, process.env.service);
+};
+
+// eslint-disable-next-line arrow-body-style
+export const getHttpHandler = async event => {
+  return httpReqHandler(
+    AWS,
+    process.env.region,
+    process.env.service,
+    process.env.accountId,
+    process.env.productId,
+    event,
+  );
+};
+
+// eslint-disable-next-line arrow-body-style
+export const postHttpHandler = async event => {
+  return httpReqHandler(
+    AWS,
+    process.env.region,
+    process.env.service,
+    process.env.accountId,
+    process.env.productId,
+    event,
+  );
 };
