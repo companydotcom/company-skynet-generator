7ca76253d3ebf7be6f90736e04259954bee6f48f
charlie <ctbwilhite@gmail.com>
2021-08-23 15:19:54 -0500

    ATX-1601 | add shared account and user data to fetch and transition workers

--- generator-companyskynet/generators/app/templates/workers/fetchWorker.js
+++ generator-companyskynet/generators/app/templates/workers/fetchWorker.js
@@ -1,25 +1,39 @@
 export default async ({
-  message, attributes, serviceConfigData, serviceAccountData, serviceUserData,
+  message,
+  attributes,
+  serviceConfigData,
+  serviceAccountData,
+  serviceUserData,
+  sharedServiceUserData,
+  sharedServiceAccountData,
 }) => {
   // Look out for user data, account data, product data in message.context
   // Service specific config data that was stored during initial setup will be
   // available in serviceConfigData. Any service specific user account data that
   // was stored earlier will be available in serviceAccountData. Similar is the
   // case with serviceUserData
-  console.log('fetchWorker: INFO: Received a call to work.');
+  console.log("fetchWorker: INFO: Received a call to work.");
   console.log(`message => ${JSON.stringify(message, null, 2)})`);
   console.log(`attributes => ${JSON.stringify(attributes, null, 2)},`);
-  console.log(`serviceConfigData => ${JSON.stringify(serviceConfigData, null, 4)}`);
-  console.log(`serviceAccountData => ${JSON.stringify(serviceAccountData, null, 4)}`);
+  console.log(
+    `serviceConfigData => ${JSON.stringify(serviceConfigData, null, 4)}`
+  );
+  console.log(
+    `serviceAccountData => ${JSON.stringify(serviceAccountData, null, 4)}`
+  );
   console.log(`serviceUserData => ${JSON.stringify(serviceUserData, null, 4)}`);
   // Write all logic to perform the required work. Create other files for other
   // helpers as necessary, import and use them while keeping this file clean.
   // Do not worry about catching errors unless necessary for the business logic.
 
   // Check if the type of fetch event is defined. Else, throw an error
-  if (typeof message.metadata === 'undefined'
-    || typeof message.metadata.eventType === 'undefined') {
-    throw new Error('Message did not have the required paameter metadata or parameter eventType within metadata');
+  if (
+    typeof message.metadata === "undefined" ||
+    typeof message.metadata.eventType === "undefined"
+  ) {
+    throw new Error(
+      "Message did not have the required paameter metadata or parameter eventType within metadata"
+    );
   }
 
   // Check what type of fetch event has been requested and perform business
@@ -28,15 +42,17 @@ export default async ({
   // fetch it later, send a parameter 'serviceAccountData' with the data to be saved
   // as a key in the return value object. Same is the case for 'serviceUserData'
   switch (message.metadata.eventType) {
-    case '':
+    case "":
       return {
-        res: 'Here is where you send the response data. This can also be an object',
+        res: "Here is where you send the response data. This can also be an object",
         serviceAccountData: {},
         serviceUserData: {},
         // extraStatus: '', // custom status to send out in addition to the default pass/ fail for custom responders to catch. Add the key only if required
         // crmData: {}, // crm data is used to pass custom data directly to Salesforce.  Add key only if required.
       };
     default:
-      throw new Error(`fetchWorker: ERROR: metadata.eventType not recognized: ${message.metadata.eventType}`);
+      throw new Error(
+        `fetchWorker: ERROR: metadata.eventType not recognized: ${message.metadata.eventType}`
+      );
   }
 };
--- generator-companyskynet/generators/app/templates/workers/transitionWorker.js
+++ generator-companyskynet/generators/app/templates/workers/transitionWorker.js
@@ -1,16 +1,26 @@
 export default async ({
-  message, attributes, serviceConfigData, serviceAccountData, serviceUserData,
+  message,
+  attributes,
+  serviceConfigData,
+  serviceAccountData,
+  serviceUserData,
+  sharedServiceUserData,
+  sharedServiceAccountData,
 }) => {
   // Look out for user data, account data, product data in message.context
   // Service specific config data that was stored during initial setup will be
   // available in serviceConfigData. Any service specific user account data that
   // was stored earlier will be available in serviceAccountData. Similar is the
   // case with serviceUserData
-  console.log('transitionWorker: INFO: Received a call to work.');
+  console.log("transitionWorker: INFO: Received a call to work.");
   console.log(`message => ${JSON.stringify(message, null, 2)})`);
   console.log(`attributes => ${JSON.stringify(attributes, null, 2)},`);
-  console.log(`serviceConfigData => ${JSON.stringify(serviceConfigData, null, 4)}`);
-  console.log(`serviceAccountData => ${JSON.stringify(serviceAccountData, null, 4)}`);
+  console.log(
+    `serviceConfigData => ${JSON.stringify(serviceConfigData, null, 4)}`
+  );
+  console.log(
+    `serviceAccountData => ${JSON.stringify(serviceAccountData, null, 4)}`
+  );
   console.log(`serviceUserData => ${JSON.stringify(serviceUserData, null, 4)}`);
 
   // Write all logic to perform the required work. Create other files for other
@@ -18,9 +28,13 @@ export default async ({
   // Do not worry about catching errors unless necessary for the business logic.
 
   // Check if the type of fetch event is defined. Else, throw an error
-  if (typeof message.metadata === 'undefined'
-    || typeof message.metadata.eventType === 'undefined') {
-    throw new Error('Message did not have the required paameter metadata or parameter eventType within metadata');
+  if (
+    typeof message.metadata === "undefined" ||
+    typeof message.metadata.eventType === "undefined"
+  ) {
+    throw new Error(
+      "Message did not have the required paameter metadata or parameter eventType within metadata"
+    );
   }
 
   // Check what type of transition event has been requested and perform business
@@ -29,15 +43,17 @@ export default async ({
   // fetch it later, send a parameter 'serviceAccountData' with the data to be saved
   // as a key in the return value object. Same is the case for 'serviceUserData'
   switch (message.metadata.eventType) {
-    case '':
+    case "":
       return {
-        res: 'Here is where you send the response data. This can also be an object',
+        res: "Here is where you send the response data. This can also be an object",
         serviceAccountData: {},
         serviceUserData: {},
         // extraStatus: '', // custom status to send out in addition to the default pass/ fail for custom responders to catch. Add the key only if required
         // crmData: {}, // crm data is used to pass custom data directly to Salesforce.  Add key only if required.
       };
     default:
-      throw new Error(`transitionWorker: ERROR: metadata.eventType not recognized: ${message.metadata.eventType}`);
+      throw new Error(
+        `transitionWorker: ERROR: metadata.eventType not recognized: ${message.metadata.eventType}`
+      );
   }
 };
